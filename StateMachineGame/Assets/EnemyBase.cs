c80152cb06eac7ac1c094e476361eea288b31205
samrose07 <samuelrose@u.boisestate.edu>
2021-09-01 11:43:34 -0600

    Sam ROse first commit

---
+++ StateMachineGame/Assets/EnemyBase.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    public enum state
+    {
+        idle,
+        move,
+        die
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
